027453521b0786554a3e8d2375390c4f1e35809e
uustoboy <tianqi_email@163.com>
2019-05-15 18:39:35 +0800

    add

--- conf/db.js
+++ conf/db.js
@@ -3,6 +3,8 @@ const env = process.env.NODE_ENV; //环境变量；
 // 配置;
 let MYSQL_CONF
 let REDIS_CONF
+let SEQUELIZE_CONF 
+
 
 if( env === 'dev' ){
     // mysql
@@ -19,14 +21,23 @@ if( env === 'dev' ){
         port: 6379,
         host: '127.0.0.1'
     }
+
+
 }
 
 if( env === 'production' ){
     // mysql
+    // MYSQL_CONF = {
+    //     host: "localhost",
+    //     user: "root",
+    //     password: "123456",
+    //     port: "3306",
+    //     database: "myblog"
+    // };
     MYSQL_CONF = {
         host: "localhost",
         user: "root",
-        password: "123456",
+        password: "",
         port: "3306",
         database: "myblog"
     };
@@ -53,10 +64,26 @@ if( env === 'winDev' ){
         port: 6379,
         host: "127.0.0.1"
     };
+
+    // Sequelize
+    SEQUELIZE_CONF = {
+        dbname: 'myblog',
+        uname: 'root',
+        upwd: '',
+        host: 'localhost',
+        port: 3306,
+        dialect: 'mysql',
+        pool: {
+            max: 5,
+            min: 0,
+            idle: 10000
+        }
+    }
 }
 
 
 module.exports = {
   MYSQL_CONF,
-  REDIS_CONF
+  REDIS_CONF,
+  SEQUELIZE_CONF
 };
